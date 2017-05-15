004f5497dbeb97a3451d35e41e47d84ed78a7ddd
fenix0111 <fenix0111@hotmail.com>
2017-05-16 02:04:42 +0900

    Add files via upload

---
+++ hello_ray.py
@@ -0,0 +1,29 @@
+nx = 500;
+ny = 400;
+
+file = open("img.ppm", "w+");
+
+file.write("P3\n");
+file.write(str(nx));
+file.write(" ");
+file.write(str(ny));
+file.write("\n255\n");
+
+j = ny - 1;
+while (j >= 0):
+    i = 0;
+    while (i < nx):
+        r = float(i) / float(nx);
+        g = float(j) / float(ny);
+        b = 0.2;
+        ir = int(255.99 * r);
+        ig = int(255.99 * g);
+        ib = int(255.99 * b);
+        file.write(str(ir));
+        file.write(" ");
+        file.write(str(ig));
+        file.write(" ");
+        file.write(str(ib));
+        file.write("\n");
+        i = i + 1;
+    j = j - 1;
